36791ad48b078081b05b4b2bda74edf750526e1b
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2025-08-17 05:35:24 +0530

    init

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  serverExternalPackages: ["@node-rs/argon2"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**", // ✅ allow all paths from utfs.io
+      },
+    ],
+  },
+  rewrites: () => {
+    return [
+      {
+        source: "/hashtag/:tag",
+        destination: "/search?q=%23:tag",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
